bc2160be2eaed77da505f0754d2f69d0e6a71f08
ZLean78 <108392755+ZLean78@users.noreply.github.com>
2024-07-02 11:38:00 -0300

    TheStoneAge 2024 Godot3.5.3
    Menú lleva a la escena 1.

--- Scenes/Menu/Menu.gd
+++ Scenes/Menu/Menu.gd
@@ -31,7 +31,7 @@ func _unhandled_input(_event):
 
 func _on_Start_pressed():
 	remove_child(Globals.settings)
-	Globals.go_to_scene("res://Scenes/Game5/Game5.tscn")
+	Globals.go_to_scene("res://Scenes/Game/Game.tscn")
 
 
 func _on_Quit_pressed():
